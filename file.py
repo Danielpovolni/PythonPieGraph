2c0d796fbee5cf1863d9c620768f04caee9ea782
Danielp <64538910+Danielpovolni@users.noreply.github.com>
2021-05-11 12:07:49 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,4 +1,14 @@
-import matplotlyb.py as plt
-
-
-percenta
+import matplotlib.pyplot as plt
+#1 tepla voda, 1 studena voda, 1 citronova stava, 3 cukor
+percenta = 100/6
+voda = percenta * 2
+citronova_stava=percenta
+cukor = percenta * 3
+percenta=[voda,citronova_stava,cukor]
+sastojci=["voda","citronova stava","cukor"]
+e = [0,0,0.1]
+plt.figure(figsize=(6,6))
+plt.pie(percenta, labels=sastojci, explode=e )
+plt.title("citronova stava sastojci v percentach")
+plt.show()
+plt.close()
