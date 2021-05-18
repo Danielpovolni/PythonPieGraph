d9f607cb58d79ecb2fd8d9f788fdd2e2f47e58c8
Danielp <64538910+Danielpovolni@users.noreply.github.com>
2021-05-18 14:57:27 +0200

    Update prsania.py

--- prsania.py
+++ prsania.py
@@ -1,1 +1,14 @@
+tyzden=[0,0,1,0,1,1,1]
 
+prsalo = round(tyzden.count(1))
+neprsalo = round(tyzden.count(0))
+
+#Pie diagram
+import matplotlib.pyplot as plt
+dazd=[prsalo, neprsalo]
+labels=["prsalo", "neprsalo"]
+plt.figure(figsize=(6,6))
+plt.pie(dazd, labels=labels)
+plt.title("kolko fni prsalo")
+plt.show()
+plt.close()
